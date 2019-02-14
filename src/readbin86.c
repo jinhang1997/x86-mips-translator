f683c348b5a5bd52772efecaeb3aeef1421b7f3c
Jin Hang <jinhang@nuaa.edu.cn>
2019-02-14 20:30:06 +0800

    feat: read symbol table

--- src/readbin86.c
+++ src/readbin86.c
@@ -8,7 +8,37 @@
 FILE *fp; /* ELF file */
 Elf32_Ehdr elf_header; /* ELF header */
 Elf32_Shdr *sh_list = NULL; /* section headers array */
+Elf32_Sym *sym_list = NULL; /* symbol table array */
+
+Elf32_Shdr *shent_symtab = NULL; /* section header of symtab */
+Elf32_Shdr *shent_strtab = NULL; /* section header of strtab */
+
 char *shstrtab = NULL; /* section headers' string table */
+char *strtab = NULL; /* symbols' string table */
+
+char *sh_type_str[] = {
+  "NULL",
+  "PROGBITS",
+  "SYMTAB",
+  "STRTAB",
+  "RELA",
+  "HASH",
+  "DYNAMIC",
+  "NOTE",
+  "NOBITS",
+  "REL",
+  "SHLIB",
+  "DYNSYM",
+  "<NOT USED>",
+  "<NOT USED>",
+  "INIT_ARRAY",
+  "FINI_ARRAY",
+  "PREINIT_ARRAY",
+  "GROUP",
+  "SYMTAB_SHNDX",
+  "NUM",
+  "<OTHER>",
+};
 
 void error(int errid, char const *word)
 {
@@ -28,6 +58,10 @@ void error(int errid, char const *word)
   else if (errid == 4)
   {
     printf("not a ELF file.\n");
+  }
+  else if (errid == 5)
+  {
+    printf("analyze error: %s\n", word);
   }
   else
   {
@@ -55,7 +89,7 @@ void read_elf_header()
 
 void print_elf_header()
 {
-  printf("Indentifiers: ");
+  printf("Identification: ");
   for (int i = 0; i < EI_NIDENT; ++i)
   {
     printf("%02x ", elf_header.e_ident[i]);
@@ -97,18 +131,20 @@ void read_section_headers()
 
 void print_section_headers()
 {
+  int sh_str_idx;
   if (!sh_list)
   {
     error(3, NULL);
   }
   printf("Section headers:\n");
-  printf("  [%2s] %-20s %-8s %-8s %-8s %-8s\n","Nr", "Name", "Type", "Address", "Offset", "Size");
+  printf("  [%2s] %-20s %-14s %-8s %-8s %-8s\n","Nr", "Name", "Type", "Address", "Offset", "Size");
   for (int i = 0; i < elf_header.e_shnum; ++i)
   {
-    printf("  [%2d] %-20s %08x %08x %08x %08x\n",
+    sh_str_idx = (sh_list[i].sh_type >= 20) ? 20 : sh_list[i].sh_type;
+    printf("  [%2d] %-20s %-14s %08x %08x %08x\n",
       i, 
       shstrtab + sh_list[i].sh_name,
-      sh_list[i].sh_type, 
+      sh_type_str[sh_str_idx], 
       sh_list[i].sh_addr, 
       sh_list[i].sh_offset, 
       sh_list[i].sh_size
@@ -130,6 +166,69 @@ Elf32_Shdr *get_shent_by_name(char const *target)
   return NULL;
 }
 
+void read_symbol_table()
+{
+  shent_symtab = get_shent_by_name(".symtab");
+  shent_strtab = get_shent_by_name(".strtab");
+  if (!shent_symtab)
+  {
+    error(5, "symtab not found");
+  }
+  if (!shent_symtab)
+  {
+    error(5, "strtab not found");
+  }
+
+  // read symbol table
+  if (fseek(fp, shent_symtab->sh_offset, SEEK_SET) != 0)
+  {
+    error(3, NULL);
+  }
+  sym_list = (Elf32_Sym *)malloc(shent_symtab->sh_size);
+  if (!sym_list)
+  {
+    error(3, NULL);
+  }
+  fread(sym_list, shent_symtab->sh_size, 1, fp);
+
+  // read names of symbols
+  if (fseek(fp, shent_strtab->sh_offset, SEEK_SET) != 0)
+  {
+    error(3, NULL);
+  }
+  strtab = (char *)malloc(shent_strtab->sh_size);
+  if (!strtab)
+  {
+    error(3, NULL);
+  }
+  fread(strtab, shent_strtab->sh_size, 1, fp);
+}
+
+void print_symbol_table()
+{
+  int i, num, ndx;
+  num = shent_symtab->sh_size / sizeof(Elf32_Sym);
+  printf("Symbol table '.symtab' contains %d entries:\n", num);
+  printf("   %3s: %8s %5s %-8s %3s %s\n", "Num", "Value", "Size", "Type", "Ndx", "Name");
+  for (i = 0; i < num; ++i)
+  {
+    ndx = sym_list[i].st_shndx;
+    if (sym_list[i].st_shndx > num)
+    {
+      ndx = -1;
+    }
+    printf("   %3d: %08x %5d %08x %3d %s\n", 
+      i,
+      sym_list[i].st_value,
+      sym_list[i].st_size,
+      ELF32_ST_TYPE(sym_list[i].st_info),
+      ndx,
+      strtab + sym_list[i].st_name
+    );
+  }
+  putchar('\n');
+}
+
 int main(int argc, char const *argv[])
 {
   if (argc < 2)
@@ -149,6 +248,9 @@ int main(int argc, char const *argv[])
 
   read_section_headers();
   print_section_headers();
+
+  read_symbol_table();
+  print_symbol_table();
 
   fclose(fp);
 
